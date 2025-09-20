3b6ccd3d09e75f4e42cc04a879c3f17e1f9efe80
LGrifyee <s22120152@alumnos.itsur.edu.mx>
2025-09-19 21:24:01 -0600

    Memoria en una función de componibilidad

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -2,6 +2,7 @@
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.basicstatecodelab.WaterCounter
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
